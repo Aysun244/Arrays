63368d777c5db6e35836a8e9a222ffa8e66c4d60
Aysun Naghizada <aysunin-bb104@code.edu.az>
2025-03-13 03:07:21 +0400

    Arrays4

--- Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Program.cs
@@ -31,19 +31,30 @@
 //    Console.WriteLine("Verilmis eded 2nin quvvetidir");
 //}
 
-int[] arr = { 22, 5, 7, 9, 23, 65 };
-int min = arr[0];
-int max = arr[0];
-for (int i = 1; i < arr.Length; i++)
+//int[] arr = { 22, 5, 7, 9, 23, 65 };
+//int min = arr[0];
+//int max = arr[0];
+//for (int i = 1; i < arr.Length; i++)
+//{
+//    if (arr[i] < min)
+//        min = arr[i];
+//    if (arr[i] > max)
+//        max = arr[i];
+//}
+//int sum = min + max;
+//{
+//    Console.WriteLine(sum);
+//}
+
+int[] arr = { 1, 3, 4, 6, 87, 23, 12 };
+int result = 0;
+for (int i=0;i<arr.Length;i++)
 {
-    if (arr[i] < min)
-        min = arr[i];
-    if (arr[i] > max)
-        max = arr[i];
+    if (arr[i] % 2 == 1 && arr[i] % 3 == 0)
+        result++;
 }
-int sum = min + max;
 {
-    Console.WriteLine(sum);
+    Console.WriteLine("Say:"+result);
 }
 
 
--- Arrays/Arrays/obj/Debug/net8.0/Arrays.AssemblyInfo.cs
+++ Arrays/Arrays/obj/Debug/net8.0/Arrays.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1582d60a1090e27c69c6573246197f383f901c38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7154da129b04580b4ceb179cb31ff8a15f966c2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
