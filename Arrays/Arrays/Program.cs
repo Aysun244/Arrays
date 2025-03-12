7154da129b04580b4ceb179cb31ff8a15f966c2d
Aysun Naghizada <aysunin-bb104@code.edu.az>
2025-03-13 02:52:39 +0400

    Arrays3

--- Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Program.cs
@@ -15,18 +15,36 @@
 //}
 
 
-int num = 56;
-int num1 = num;
-while (num1 > 1)
+//int num = 56;
+//int num1 = num;
+//while (num1 > 1)
+//{
+//    if (num1 % 2 != 0)
+//    {
+//        Console.WriteLine("Verilmis eded 2nin quvveti deyil");
+//        return;
+//    }
+//    num1 /= 2;
+//}
+
+//{
+//    Console.WriteLine("Verilmis eded 2nin quvvetidir");
+//}
+
+int[] arr = { 22, 5, 7, 9, 23, 65 };
+int min = arr[0];
+int max = arr[0];
+for (int i = 1; i < arr.Length; i++)
 {
-    if (num1 % 2 != 0)
-    {
-        Console.WriteLine("Verilmis eded 2nin quvveti deyil");
-        return;
-    }
-    num1 /= 2;
+    if (arr[i] < min)
+        min = arr[i];
+    if (arr[i] > max)
+        max = arr[i];
+}
+int sum = min + max;
+{
+    Console.WriteLine(sum);
 }
 
-{
-    Console.WriteLine("Verilmis eded 2nin quvvetidir");
-}
+
+
